57101b6161b545edb49fb225ecf626be4d9f4464
nachozbrun <ignazbrun5@gmail.com>
2022-04-03 15:14:55 -0300

    Faltan Punto 3, 6, 13, 14 y 15

--- PruebaNuevosConceptos-Zbrun-/PruebaNuevosConceptos-Zbrun-/Utilidades.cs
+++ PruebaNuevosConceptos-Zbrun-/PruebaNuevosConceptos-Zbrun-/Utilidades.cs
@@ -1,4 +1,5 @@
-﻿namespace PruebaNuevosConceptos_Zbrun_
+﻿using System.Text.RegularExpressions;
+namespace PruebaNuevosConceptos_Zbrun_
 {
     public static class Utilidades
     {
@@ -56,6 +57,59 @@
                 resta = resta - impares[i];
             }
             return resta;
+        }
+
+        //Punto 6 : Crear un método de extensión que retorne una fecha en formato DateTime a partir de un string
+        //(si se genera una excepción por mal formato retornar el MinValue del tipo de datos).
+        //public static DateTime RetornarDatetimeAPartirDeString(this string Fecha)
+        //{
+        //    string[] fechas = Fecha.Split(' ');
+        //    DateTime fechaRetornar = new DateTime(fechas[0], fechas[1], fechas[2]);
+        //    return fechaRetornar;
+        //}
+
+        //Punto 7
+        public static string RetornarStringInvertido(this string mensaje)
+        {
+            string mensajeInvertido = "";
+            foreach (char letra in mensaje)
+            {
+                mensajeInvertido = letra + mensajeInvertido;
+            }
+            return mensajeInvertido;
+        }
+
+        //Punto 8 
+        public static string RetornarArrayStringsComoString(string[] strings, char separador)
+        {
+            string retornarString = "";
+            foreach (string elemento in strings)
+            {
+                retornarString = retornarString + separador + elemento;
+            }
+            return retornarString;
+        }
+
+        //Punto 9 
+        public static bool EsCorreoElectronico(string correo)
+        {
+            string patronCorreo = @"^[a-zA-Z\.\-_]+@^([a-zA-Z\.\-_]+\.)+[a-zA-Z{2,4}]$";
+            Regex rgxCorreo = new Regex(patronCorreo);
+            return (rgxCorreo.IsMatch(correo));
+        }
+
+        //Punto 10  
+        public static bool EsCuit(string cuit)
+        {
+            string patronCuit = @"^([20|23|24|27|30|33|34])([0-9]{8})([0-9]{1})";
+            Regex rgxCuit = new Regex(patronCuit);
+            return rgxCuit.IsMatch(cuit);
+        }
+
+        //Punto 13 : Crear un método de extensión que reste dos números enteros.
+        public static int RetornarRestaDosEnteros(this int entero1, int entero2)
+        {
+            return entero1 - entero2;
         }
     }
 }
